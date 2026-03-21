fc87b6032d5b204431d183b66b4b4fc1b54ccba2
abubakarva33 <abubakarsiddikl.info@gmail.com>
2026-03-22 00:21:43 +0600

    update

--- data/site.ts
+++ data/site.ts
@@ -3,7 +3,7 @@ export const siteConfig = {
   phone: "+447300963972", // Using from prototype as it looks active
   email: "info@nfullerspavingandimprovements.co.uk", // Derived from BRAND.md domain
   address: "New Buildings Farm, Parish Ln RH11 9AA",
-  domain: "www.nfullerspavingandimprovements.co.uk",
+  domain: "https://www.nfullerspavingandimprovements.co.uk",
   colors: {
     primary: "#1F3F77", // Blue
     accent: "#E46C1A",  // Orange
